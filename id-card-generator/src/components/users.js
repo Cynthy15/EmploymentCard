f6ab0a049b912685c38ecc99a5eb3dc33b9f5789
Cynthy Niganze <cynthynigaira@gmail.com>
2026-03-05 12:27:54 +0200

    adding images

--- id-card-generator/src/components/users.js
+++ id-card-generator/src/components/users.js
@@ -1,24 +1,24 @@
 const people = [
   {
-    id: 1,
+    id:"EMP001",
     name: "Alice Johnson",
     role: "Software Engineer",
     department: "Engineering",
-    avatar: "https://randomuser.me/api/portraits/women/44.jpg",
+    avatar: "https://images.unsplash.com/photo-1723179343134-634c9eadde39?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlJTIwcHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww",
   },
   {
-    id: 2,
+    id: "EMP002",
     name: "Michael Smith",
     role: "Product Manager",
     department: "Product",
-    avatar: "https://randomuser.me/api/portraits/men/46.jpg",
+    avatar: "https://media.istockphoto.com/id/1406197730/photo/portrait-of-a-young-handsome-indian-man.webp?a=1&b=1&s=612x612&w=0&k=20&c=MfdvUjMRNMRgW8DfvlEMiFjm8vcJW_2BC2FFWcm2unI=",
   },
   {
-    id: 3,
-    name: "Sophia Williams",
+    id: "EMP003",
+    name: "cynthy",
     role: "UI/UX Designer",
     department: "Design",
-    avatar: "https://randomuser.me/api/portraits/women/68.jpg",
+    avatar: "https://plus.unsplash.com/premium_photo-1687188211060-8f2f6d996697?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlJTIwcHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww",
   },
 ];
 
